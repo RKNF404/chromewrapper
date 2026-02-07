40f3e5a511c55ba1ddb5ceeb13e8ec453e3f92b2
Rootkit404 <175176948+RKNF404@users.noreply.github.com>
2026-02-07 00:16:07 -0500

    Update chromewrapper.bat

--- chromewrapper.bat
+++ chromewrapper.bat
@@ -3,7 +3,7 @@
 setlocal EnableDelayedExpansion
 set "CHROMEWRAPPER_FLAGS=--start-maximized --no-default-browser-check"
 
-for /f "usebackq delims=" %%L in ("C:\Program Files\Google\Chrome\Application\hardening-guide-flags.conf") do (
+for /f "usebackq delims=" %%L in ("C:\Program Files\Google\Chrome\Application\80-hardening-guide-flags.conf") do (
   set "CHROMEWRAPPER_FLAGS=!CHROMEWRAPPER_FLAGS! %%L"
 )
 
